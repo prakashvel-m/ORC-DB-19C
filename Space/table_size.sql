d0f1927f686e317bfc275e09c20650198866323a
prakashvel-m <123497509+prakashvel-m@users.noreply.github.com>
2024-08-13 19:44:21 +0530

    Update table_size.sql

--- Space/table_size.sql
+++ Space/table_size.sql
@@ -3,3 +3,30 @@ SELECT segment_name AS table_name,
 FROM dba_segments 
 WHERE segment_type = 'TABLE' 
 AND segment_name like 'FND_LOOKUP_VALUES%'
+
+
+
+SELECT 
+    o.owner,
+    o.object_name,
+    e.segment_name,
+    o.object_type,
+    e.segment_type,
+    e.tablespace_name,
+    SUM(e.bytes) / (1024 * 1024 * 1024) AS total_size_in_gb
+FROM 
+    dba_extents e
+JOIN 
+    dba_objects o
+ON 
+    e.owner = o.owner AND e.segment_name = o.object_name
+WHERE 
+    o.object_type = 'TABLE'
+    AND o.object_name = 'OE_ORDER_LINES_ALL'
+GROUP BY 
+    o.owner,
+    o.object_name,
+    e.segment_name,
+    e.segment_type,
+    o.object_type,
+    e.tablespace_name;
