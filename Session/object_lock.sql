5de1053c61f379d5495c5e5a1b6a861bc5f210b3
prakashvel-m <123497509+prakashvel-m@users.noreply.github.com>
2024-08-21 18:53:24 +0530

    Update object_lock.sql

--- Session/object_lock.sql
+++ Session/object_lock.sql
@@ -52,3 +52,58 @@ where
    b.sid = a.session_id
 and
    a.object_id = c.object_id and object_name in ('OBJECT_NAME');
+
+
+
+
+SELECT
+    s.inst_id,
+    l.sid,
+    s.serial#,
+    nvl(
+        s.username, 'Internal'
+    )                 username,
+    s.event,
+    s.state,
+    decode(
+        s.command, 0, 'None', decode(
+            l.id2, 0, u1.name
+                      || '.'
+                      || substr(
+                t1.name, 1, 20
+            ), 'None'
+        )
+    )                 tab,
+    c.command_name    command,
+    decode(
+        l.lmode, 1, 'No Lock', 2, 'Row Share', 3, 'Row Exclusive', 4, 'Share', 5, 'Share Row Exclusive', 6, 'Exclusive', '--none--'
+    )                 lmode,
+    decode(
+        l.request, 1, 'No Lock', 2, 'Row Share', 3, 'Row Exclusive', 4, 'Share', 5, 'Share Row Exclusive', 6, 'Exclusive', '--none--'
+    )                 request,
+    l.id1
+    || '-'
+    || l.id2          laddr,
+    l.type
+    || ' - '
+    || lt.description lockt
+FROM
+    gv$lock       l,
+    gv$session    s,
+    sys.user$    u1,
+    sys.obj$     t1,
+    v$sqlcommand c,
+    v$lock_type  lt
+WHERE
+    l.sid = s.sid
+    AND l.inst_id = s.inst_id
+    AND t1.obj# = decode(
+        l.id2, 0, l.id1, 1
+    )
+    AND u1.user# = t1.owner#
+    AND s.type != 'BACKGROUND'
+    AND c.command_type = s.command
+    AND lt.type = l.type
+ORDER BY
+    s.inst_id,
+    s.sid;
